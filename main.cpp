dc34f0ec99704ff93d3d38bb2ed0bcbef4ba76aa
246319 <246319@edu.p.lodz.pl>
2022-10-03 11:16:58 +0200

    metoda mnożenia

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+int Licz(int a, int b)
+{
+    return a*b;
+}
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "W pierwszym branchu; 4*5=" << Licz(4,5) << std::endl;
     return 0;
 }
